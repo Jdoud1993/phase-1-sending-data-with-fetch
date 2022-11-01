ef7bf6f47e42ba14565d17e294360edd58f26653
Joshua Doud <bigjj3393@yahoo.com>
2022-11-01 15:26:33 -0700

    final

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    fetch(`http://localhost:3000/users`, {
+        method: `POST`,
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body:JSON.stringify({name:userName, email:userEmail})
+    })
+    .then((resp) => resp.json())
+    .then((data) => appendData(data.id))
+    .catch((err) => appendData(err))    
+}
+
+function appendData(data) {
+    const body = document.querySelector(`body`)
+    body.innerText = `${data}`
+    
+}
+
+
